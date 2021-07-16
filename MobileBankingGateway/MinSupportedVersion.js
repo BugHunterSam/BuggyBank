7a04b35a4d2a28e0e7318890a7659052703f4ec9
Sam <sam@thebughunter.com.au>
2021-07-16 13:24:10 +1000

    added minsupportversion API

---
+++ MobileBankingGateway/MinSupportedVersion.js
@@ -0,0 +1,27 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+//var http = require('http');
+var minAndriodVersion = "1.0.0"
+var minIOSVersion = "2.1.0"
+
+exports.minVersion = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion}));
+    res.status(200);
+};
+
+exports.minVersionWithScheduledMessage = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion, "scheduledMessage": "Our service will be down this Friday from 8pm AEST to 10pm" }));
+    res.status(200);
+};
+
+exports.minVersionWithOutageMessage = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion,  "outageMessage": "We are out of action until 10pm AEST, please try again later" }));
+    res.status(200);
+};
