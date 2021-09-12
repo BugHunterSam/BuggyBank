5079aca5a1624f4940a79204dad11610175a87c5
Sam Connelly <sam@thebughunter.com.au>
2021-09-13 09:34:20 +1000

    fixed typo

--- MobileBankingGateway/MinSupportedVersion.js
+++ MobileBankingGateway/MinSupportedVersion.js
@@ -5,23 +5,23 @@
  * @param {!express:Response} res HTTP response context.
  */
 //var http = require('http');
-var minAndriodVersion = "1.0.0"
+var minAndroidVersion = "1.0.0"
 var minIOSVersion = "2.1.0"
 
 exports.minVersion = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion}));
+    res.end(JSON.stringify({ "Android": minAndroidVersion, "iOS": minIOSVersion}));
     res.status(200);
 };
 
 exports.minVersionWithScheduledMessage = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion, "scheduledMessage": "Our service will be down this Friday from 8pm AEST to 10pm" }));
+    res.end(JSON.stringify({ "Android": minAndroidVersion, "iOS": minIOSVersion, "scheduledMessage": "Our service will be down this Friday from 8pm AEST to 10pm" }));
     res.status(200);
 };
 
 exports.minVersionWithOutageMessage = (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "Android": minAndriodVersion, "iOS": minIOSVersion,  "outageMessage": "We are out of action until 10pm AEST, please try again later" }));
+    res.end(JSON.stringify({ "Android": minAndroidVersion, "iOS": minIOSVersion,  "outageMessage": "We are out of action until 10pm AEST, please try again later" }));
     res.status(200);
 };
